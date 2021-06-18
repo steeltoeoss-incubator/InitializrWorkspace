7abe7d16624e32a1bb1b039820d893eae825e2b9
Chris Cheetham <chris@cheetham.com>
2021-06-18 09:04:26 -0400

    Clone project repository if not found in parent

--- initialize-workspace.ps1
+++ initialize-workspace.ps1
@@ -5,19 +5,28 @@
 Initializes the Initializr workspace.
 
 .DESCRIPTION
-Initializes the workspace by creating soft links to the Initializr projects.
-If a project path already exists, that project is skipped.
+Initializes the workspace by creating paths to the Initializr projects.
+For each project:
+  * if a project path already exists, that project is skipped
+  * if a project is found in the parent directory, a soft link to the project is created
+  * otherwise, the project is cloned
 #>
 
 $baseDir = "$PSScriptRoot"
 $projects = "NetCoreToolTemplates", "NetCoreToolService", "InitializrConfig", "InitializrConfigServer", "InitializrApi", "InitializrWeb"
 
+Set-Location -Path $baseDir
 Foreach ($project in $projects) {
     $projectPath = Join-Path -Path $baseDir -ChildPath $project
     if (Test-Path -Path $projectPath) {
         "skipping $project"
         continue
     }
-    "linking $project"
-    New-Item -ItemType SymbolicLink -Path "$projectPath" -Target "../$project" | Out-Null
+    if (Test-Path -Path "../$project") {
+        "linking $project"
+        New-Item -ItemType SymbolicLink -Path "$projectPath" -Target "../$project" | Out-Null
+        continue
+    }
+    "cloning $project"
+    git clone https://github.com/SteeltoeOSS/$project 2>&1 | Out-Null
 }

84bc34be0aa9f4493ac80b7e40718fe4f638b0c1
Chris Cheetham <chris@cheetham.com>
2021-06-18 09:22:54 -0400

    Add support for cloning over SSH

--- initialize-workspace.ps1
+++ initialize-workspace.ps1
@@ -10,7 +10,12 @@ For each project:
   * if a project path already exists, that project is skipped
   * if a project is found in the parent directory, a soft link to the project is created
   * otherwise, the project is cloned
+
+To use SSH instead of HTTPS, pass the -ssh switch.
 #>
+Param (
+    [switch]$Ssh = $false # Use SSH instead of HTTPS to clone Git repositories
+)
 
 $baseDir = "$PSScriptRoot"
 $projects = "NetCoreToolTemplates", "NetCoreToolService", "InitializrConfig", "InitializrConfigServer", "InitializrApi", "InitializrWeb"
@@ -23,10 +28,11 @@ Foreach ($project in $projects) {
         continue
     }
     if (Test-Path -Path "../$project") {
-        "linking $project"
+        "linking ../$project"
         New-Item -ItemType SymbolicLink -Path "$projectPath" -Target "../$project" | Out-Null
         continue
     }
-    "cloning $project"
-    git clone https://github.com/SteeltoeOSS/$project 2>&1 | Out-Null
+    $projectUrl = "$($ssh ? "git@" : "https://")github.com:/SteeltoeOSS/$project"
+    "cloning $projectUrl"
+    git clone $projectUrl 2>&1 | Out-Null
 }
